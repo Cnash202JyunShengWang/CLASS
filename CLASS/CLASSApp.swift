008e12b3959ce5798bd6556ba44f3f31a4024673
Roy <a0966750212@icloud.com>
2021-10-03 09:47:20 +0800

    Initial Commit

---
+++ CLASS/CLASSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CLASSApp.swift
+//  CLASS
+//
+//  Created by 暨大附中 on 2021/10/3.
+//
+
+import SwiftUI
+
+@main
+struct CLASSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
